395ea8c9498e70317fb027bbbb7af172e362c8e7
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-08-31 21:13:48 +0430

    organizing folders' structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl text-center text-red-500">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
