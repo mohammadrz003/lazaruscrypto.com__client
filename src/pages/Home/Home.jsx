5d95247fb880098ef6324301691e409a0121dcf8
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-09-02 17:23:38 +0430

    add eyes follow mouse to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
 import Layout from "../../layouts/Layout";
+import EyesFollowMouse from "./EyesFollowMouse";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <Layout>
-      <div className="flex px-20 py-6 h-full">
+      <div className="flex px-20 py-6 h-full relative">
+        <div className={styles.blurCircle1} />
         <div className="lg:w-1/2 h-full flex flex-col justify-center">
           <h2 className="font-astraneo lg:leading-none lg:text-9xl xl:text-[9rem]">
             MARKET
           </h2>
+          <EyesFollowMouse />
         </div>
-        <div className="lg:w-1/2"></div>
+        <div className="lg:w-1/2 h-full"></div>
       </div>
     </Layout>
   );
