789a3a60072494228707bae000db4e61b3d6d8a7
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-09-15 15:03:17 +0430

    add members' online status logic

--- src/components/common/OnlineStatus.jsx
+++ src/components/common/OnlineStatus.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { MdPerson } from "react-icons/md";
 
 const OnlineStatus = () => {
+  const [onlineMembersNumber, setOnlineMembersNumber] = useState(0);
+  const [randomNumber, setRandomNumber] = useState(Math.random());
+
+  useEffect(() => {
+    var d = Math.random();
+    if (d < 0.34) {
+      // 34% chance of being here
+      setOnlineMembersNumber(Math.round(Math.random() * 500 + 500));
+    } else if (d < 0.58) {
+      // 24% chance of being here
+      setOnlineMembersNumber(Math.round(Math.random() * 500 + 1000));
+    } else if (d < 0.77) {
+      // 19% chance of being here
+      setOnlineMembersNumber(Math.round(Math.random() * 500 + 1500));
+    } else if (d < 0.91) {
+      // 14% chance of being here
+      setOnlineMembersNumber(Math.round(Math.random() * 500 + 2000));
+    } else {
+      // 9% chance of being here
+      setOnlineMembersNumber(Math.round(Math.random() * 500 + 2500));
+    }
+  }, [randomNumber]);
+
   return (
-    <div className="fixed bottom-[5%] right-[5%] lg:right-[2.5%] z-[1000]">
+    <div
+      onClick={() => setRandomNumber(Math.random())}
+      className="fixed bottom-[5%] right-[5%] lg:right-[2.5%] z-[1000]"
+    >
       <div class="indicator tab tab-lifted tab-active rounded-md">
         <MdPerson />
-        120
+        {onlineMembersNumber}
         <span class="indicator-item badge h-3 pl-1 pr-1.5 bg-palette-green"></span>
       </div>
     </div>
